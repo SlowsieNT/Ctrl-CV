071645af6083bbd0a1a28b99c5d65f59980ba35a
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2021-09-12 18:52:53 +0000

    Create JsonWrapper.cs

---
+++ src/JsonWrapper.cs
@@ -0,0 +1,18 @@
+// Newtonsoft JSON?? No no no, we dont use needless things
+public class JSON { // System.Web.Extensions.dll
+	static JavaScriptSerializer m_JSS = new JavaScriptSerializer();
+	public static string Stringify<T>(T aObject) {
+		try {
+			return m_JSS.Serialize(aObject);
+		} catch {
+			return "";
+		}
+	}
+	public static T Parse<T>(string aString) {
+		try {
+			return m_JSS.Deserialize<T>(aString);
+		} catch {
+			return default(T);
+		}
+	}
+}
