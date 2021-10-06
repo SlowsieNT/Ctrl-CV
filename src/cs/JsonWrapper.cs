d988ade795822fe12951b7cdb0895a97b391f73c
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2021-10-06 10:45:44 +0000

    Update JsonWrapper.cs

--- src/cs/JsonWrapper.cs
+++ src/cs/JsonWrapper.cs
@@ -1,19 +1,9 @@
-using System.Web.Script.Serialization;
-// Newtonsoft JSON?? No no no, we dont use needless things
-public class JSON { // ref dll: System.Web.Extensions.dll
-	static JavaScriptSerializer m_JSS = new JavaScriptSerializer();
-	public static string Stringify<T>(T aObject) {
-		try {
-			return m_JSS.Serialize(aObject);
-		} catch {
-			return "";
-		}
-	}
+public class JSON { // System.Web.Extensions.dll
+	public static System.Web.Script.Serialization.JavaScriptSerializer m_JSS = new System.Web.Script.Serialization.JavaScriptSerializer();
 	public static T Parse<T>(string aString) {
-		try {
-			return m_JSS.Deserialize<T>(aString);
-		} catch {
-			return default(T);
-		}
+		try { return m_JSS.Deserialize<T>(aString); } catch { return default(T); }
+	}
+	public static string ToString<T>(T aObject) {
+		try { return m_JSS.Serialize(aObject); } catch { return ""; }
 	}
 }
