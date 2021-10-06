4a68b33b1a17b335b929cf13d7fe2ec66b175d15
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2021-10-06 10:48:18 +0000

    Create TimeUtils.cs

---
+++ src/cs/TimeUtils.cs
@@ -0,0 +1,8 @@
+public class TimeUtils {
+	public static double GetSec(double aAdd = 0) {
+		return Get(aAdd) / 1000.0;
+	}
+	public static double Get(double aAdd = 0) {
+		return (aAdd + (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds);
+	}
+}
